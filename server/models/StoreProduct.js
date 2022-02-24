95540a8e1b96800dbbb3c45ae0f6d125f50b9b82
Srikanth <sri1@publicisgroupe.net>
2022-02-24 16:20:19 +0530

    store product model added for mongodb

---
+++ server/models/StoreProduct.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const StoreProductSchema = new mongoose.Schema({
+    projectName: {
+        type: String,
+        required: true,
+    },
+    url: {
+        type: String,
+        required: true,
+    },
+    technologies: {
+        type: Array,
+        required: true,
+    },
+    githubUrl: {
+        type: String,
+        required: true,
+    },
+    thumbnail: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    featured: {
+        type: Boolean,
+        default: false,
+    },
+    description: {
+        type: String,
+        default: "Hi There, I'm Srikanth and I am Front-End Web Developer. It's been 2 years, I'm working as a web developer. I have developed many website and also sharing my knowledge to college students."
+    }
+});
+
+module.exports = mongoose.model('StoreProduct', StoreProductSchema)
